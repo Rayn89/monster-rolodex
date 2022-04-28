b0a12b6d02b055d226105c043a15c5a2d5e75182
Raymond Nehring <rnehring89@gmail.com>
2022-04-28 16:36:23 -0500

    Completed part 27

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Ray!"
+    }
+  }
 
   render() {
     return (
@@ -11,16 +18,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button>
+            Change name
+          </button>
         </header>
       </div>
     );
