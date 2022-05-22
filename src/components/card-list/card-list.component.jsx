89472962ad2dffbefc22a77b00b85bf1ebcfba1e
Raymond Nehring <rnehring89@gmail.com>
2022-05-22 09:54:00 -0500

    49 Complete - CardList Component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,20 @@
 import { Component } from 'react';
+import './card.styles.css'
 
 class CardList extends Component {
     render() {
         const { monsters } = this.props;
         return(
             <div className="card-list">
-            {monsters.map((monster) => (
-                <h1 key={monster.id}>{monster.name}</h1>
-            ))}
+            {monsters.map((monster) => {
+            const {name, email, id} = monster;
+            return (
+                <div className='card-container' key={id}>
+                    <img alt={`${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
+            )})}
             </div>
         )
     }
